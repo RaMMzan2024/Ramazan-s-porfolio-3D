0346e8e8d9e95922c0651ed05ef28da79d9153bd
Ramazan <148753896+RaMMzan2024@users.noreply.github.com>
2024-05-11 18:24:56 +0600

    changed emailjs things

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -36,8 +36,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        import.meta.env.service_4971e32,
+        import.meta.env.template_wzvkvps,
         {
           from_name: form.name,
           to_name: "Jagadeesh Koyya",
